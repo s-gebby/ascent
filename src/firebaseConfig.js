9691953ec57b0dae98b512e127ad83636f6da785
silas <silasgebhart12@gmail.com>
2024-09-18 10:15:04 -0500

    adding .env file and changes to firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVDVolpCHMWq5NjY3fZDvirc6MO-pdFrE",
-  authDomain: "ascend-c3234.firebaseapp.com",
-  projectId: "ascend-c3234",
-  storageBucket: "ascend-c3234.appspot.com",
-  messagingSenderId: "135739512372",
-  appId: "1:135739512372:web:889088084af719f1db1c61",
-  measurementId: "G-Q0FL3TSC8R"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
